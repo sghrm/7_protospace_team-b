0b172e148c3af9728c6bcb88055eafb81a377798
kojisagawa <sagawa@glocal-food.co.jp>
2018-08-25 16:29:47 +0900

    Edit-controller-prototype-before_action,edit,update

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: :show
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
     @prototypes = Prototype.all
@@ -20,6 +20,16 @@ class PrototypesController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.user_id == current_user.id
+      @prototype.update(prototype_params)
+      redirect_to action: :show
+    end
   end
 
   private
