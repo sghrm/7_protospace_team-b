7c03f50786239cc12281dc5bf7dd7a96fef0475c
kojisagawa <sagawa@glocal-food.co.jp>
2018-09-15 16:30:02 +0900

    Edit-controller-add_json_format

--- app/controllers/newest_controller.rb
+++ app/controllers/newest_controller.rb
@@ -2,6 +2,10 @@ class NewestController < ApplicationController
 
   def index
     @prototypes = Prototype.all.order(created_at: :DESC)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
 end
