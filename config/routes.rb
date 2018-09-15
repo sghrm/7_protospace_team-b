e03fe5ce858cffd98c234ccb9e898f9325fc7003
kojisagawa <sagawa@glocal-food.co.jp>
2018-09-15 11:50:52 +0900

    Add-routes-popular,newest

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
+  resources :newest, only: :index
+  resources :popular, only: :index
 end
