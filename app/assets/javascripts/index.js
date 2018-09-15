81c9bfb34af75fa77896cad402315fc9c0d370a5
kojisagawa <sagawa@glocal-food.co.jp>
2018-09-15 18:01:09 +0900

    Edit-js-class

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,27 +2,26 @@ $(function() {
 
   function buildHTML(data) {
     var html =
-    `<div class="col-sm-4.col-md-3.proto-content">
+    `<div class="col-sm-4 col-md-3 proto-content">
       <div class="thumbnail">
-        <a href="/prototype/${data.id}">
-          <img src="${data.set_main_thumbnail}">
+        <a href="/prototypes/${data.id}">
+          <img src="${data.main.url}">
         </a>
         <div class="caption">
           <h3>${data.title}</h3>
           <div class="proto-meta">
             <div class="proto-user">
               <a href="/users/${data.user_id}">
+              ${data.user_name}
             </div>
             <div class="proto-posted">
-              ${data.created_at}
+              ${data.posted_date}
             </div>
           </div>
         </div>
       </div>
     </div>`
-    console.log(html);
-    $('#masonry-container').append(html);
-    // return html;
+    return html;
   }
 
   $('.btn-newest').on('click', function() {
@@ -31,11 +30,14 @@ $(function() {
       type: "GET",
       dataType: 'json'
     })
-    .done(function(data) {
-      console.log(data);
-      $('#masonry-container').remove();
-      var html = buildHTML(data);
-      // $('.proto-list').append(html);
+    .done(function(json) {
+      $('#masonry-container').empty();
+      json.forEach(function(data) {
+        var html = '';
+        html += buildHTML(data);
+      $('#masonry-container').append(html);
+      console.log(html);
     })
+    });
   });
 });
