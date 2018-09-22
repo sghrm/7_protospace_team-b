a0510e721acda087d2ddbff3b15e74a8ea7f678f
福森賢 <kenkenken0328@icloud.com>
2018-09-22 11:45:29 +0900

    Sava comment
    ルーティングのネスト
    バリデーションの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
+  def create
+    #binding.pry
+  Comment.create(comment_params)
+    #binding.pry
+    redirect_to root_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :text).merge(prototype_id: params[:prototype_id])
   end
 end
