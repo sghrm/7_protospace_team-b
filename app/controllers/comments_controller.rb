e837c7af8d6fc6ddbd4b863d80f03c1ecf2cf8b1
福森賢 <kenkenken0328@icloud.com>
2018-09-22 14:59:39 +0900

    名前の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:user_id, :text).merge(prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:user_id, :text).merge(prototype_id: params[:prototype_id],user_id: current_user.id)
   end
 end
